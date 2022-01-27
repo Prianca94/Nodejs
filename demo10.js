19309816a319d5ac03db384cb65f8cc34ba7f174
Priyanka Lalwani <98509876+Prianca94@users.noreply.github.com>
2022-01-27 12:03:28 +0530

    Add files via upload

---
+++ demo10.js
@@ -0,0 +1,72 @@
+   
+  /* let selectedNames = ['john','sam'];
+    selectedNames[2]=1;
+        console.log(selectedNames.length);
+
+    let person = {
+
+        name:'john',
+        age:30
+    };
+//person.name='nike';
+
+person['name']='lusi';
+console.log(person);*/
+
+/*let hour=17;
+if(hour>=6 && hour<12)
+{
+    console.log("good morning");
+
+}
+else if(hour>12 && hour<18)
+{
+    console.log("Good afternoon");
+}
+else{
+
+    console.log("good evening");
+
+}*/
+
+/*let role="guest";
+
+switch(role)
+{
+    case "guest": console.log("Welcome guest");
+    break;
+
+    case "moderator" : console.log("Welcome moderator");
+    break;
+
+    default: console.log("Unknown user");
+}*/
+
+/*for(let i=0; i<=5;i++)
+{
+    if(i%2==0)
+    console.log("Good morning" , i);
+}*/
+
+/*let i=0;
+while(i<=10)
+{
+    if(i%2==0)
+       console.log(i);
+       i++;
+}*/
+
+/*function demo()
+{
+    alert("Hello This javascript demo with different js file");
+}
+*/
+
+
+
+
+
+
+
+
+
